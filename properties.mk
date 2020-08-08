1fbf4a13a73f3d528037d5479b943d72e2d0132c
4PERTURE <slicey2001@gmail.com>
2020-08-08 17:03:19 +0000

    [TEST] Fix portrait on ANXCamera

--- properties.mk
+++ properties.mk
@@ -112,6 +112,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.preview.ubwc=0 \
     vendor.video.disable.ubwc=1 \
     vidc.enc.dcvs.extra-buff-count=2
+    vendor.camera.aux.packagelist=com.android.camera
 
 # CNE
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
